c223e32df0067d088c27be4e4b9be5a00d8339c5
person124 <cmacleodp@gmail.com>
2013-10-08 12:01:21 -0400

    Create EntityLimboSage.java

---
+++ com/person124/sm/entity/EntityLimboSage.java
@@ -0,0 +1,87 @@
+package com.person124.sm.entity;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.ai.EntityAIAttackOnCollide;
+import net.minecraft.entity.ai.EntityAIBreakDoor;
+import net.minecraft.entity.ai.EntityAIHurtByTarget;
+import net.minecraft.entity.ai.EntityAILookIdle;
+import net.minecraft.entity.ai.EntityAIMoveTowardsRestriction;
+import net.minecraft.entity.ai.EntityAINearestAttackableTarget;
+import net.minecraft.entity.ai.EntityAISwimming;
+import net.minecraft.entity.ai.EntityAIWander;
+import net.minecraft.entity.ai.EntityAIWatchClosest;
+import net.minecraft.entity.monster.EntityMob;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.world.World;
+
+import com.person124.sm.element.Limbo;
+
+public class EntityLimboSage extends EntityMob {
+
+	public EntityLimboSage(World world) {
+		super(world);
+		this.getNavigator().setAvoidsWater(true);
+		this.setSize(1.0F, 2.0F);
+
+		this.tasks.addTask(0, new EntityAISwimming(this));
+        this.tasks.addTask(1, new EntityAIBreakDoor(this));
+        this.tasks.addTask(2, new EntityAIAttackOnCollide(this, 1.0D, false));
+        this.tasks.addTask(3, new EntityAIMoveTowardsRestriction(this, 1.0D));
+        this.tasks.addTask(4, new EntityAIWander(this, 1.0D));
+        this.tasks.addTask(5, new EntityAIWatchClosest(this, EntityPlayer.class, 8.0F));
+        this.tasks.addTask(6, new EntityAILookIdle(this));
+        this.targetTasks.addTask(1, new EntityAIHurtByTarget(this, true));
+        this.targetTasks.addTask(2, new EntityAINearestAttackableTarget(this, EntityPlayer.class, 0, true));
+	}
+
+	protected void applyEntityAttributes() {
+		super.applyEntityAttributes();
+		this.getEntityAttribute(SharedMonsterAttributes.followRange).setAttribute(40.0D);
+		this.getEntityAttribute(SharedMonsterAttributes.movementSpeed).setAttribute(0.23000000417232513D);
+		this.getEntityAttribute(SharedMonsterAttributes.attackDamage).setAttribute(3.0D);
+		this.getEntityAttribute(SharedMonsterAttributes.maxHealth).setAttribute(30.0D);
+	}
+
+	public boolean isAIEnabled() {
+		return true;
+	}
+
+	protected String getLivingSound() {
+		return "mob.zombie.say";
+	}
+
+	protected String getHurtSound() {
+		return "mob.zombie.hurt";
+	}
+
+	protected String getDeathSound() {
+		return "mob.zombie.death";
+	}
+
+	protected int getDropItemId() {
+		return 0;
+	}
+
+	public int getAttackStrength(Entity entity) {
+		return 2;
+	}
+
+	@Override
+	public boolean attackEntityAsMob(Entity entity) {
+			if (entity instanceof EntityPlayer) {
+				EntityPlayer player = (EntityPlayer) entity;
+
+				player.addPotionEffect(new PotionEffect(Potion.blindness.id, 100, 0, true));
+				player.heal(-2);
+			}
+		return true;
+	}
+	
+	public void onDeathUpdate() {
+		if(!this.worldObj.isRemote)this.dropItem(Limbo.limboPearl.itemID, 4);
+		this.setDead();
+	}
+}
