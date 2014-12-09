7db834782fb23b9630fc66c564680f04bdd2eb18
person124 <cmacleodp@gmail.com>
2014-12-09 14:52:42 +0000

    Create RenderCustomMob.java
    This is just a test to see if I can do this in just one file, unfortunately I can't test it until I get home, so it might be awhile until the next update.

---
+++ com/person124/sm/render/RenderCustomMob.java
@@ -0,0 +1,40 @@
+package com.person124.sm.render;
+
+import net.minecraft.client.model.ModelBase;
+import net.minecraft.client.renderer.entity.RenderLiving;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLiving;
+import net.minecraft.util.ResourceLocation;
+
+import com.person124.sm.entity.EntityEarthSage;
+import com.person124.sm.entity.EntityFireSage;
+import com.person124.sm.entity.EntityLimboSage;
+
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
+@SideOnly(Side.CLIENT)
+public class RenderCustomMob extends RenderLiving {
+	public RenderCustomMob(ModelBase model, float shadowSize) {
+		super(model, shadowSize);
+	}
+	
+	public void renderEntity(EntityLiving entity, double x, double y, double z, float pich, float yaw) {
+		super.doRender(entity, x, y, z, pitch, yaw);
+	}
+
+	public void doRenderLiving(EntityLiving entity, double x, double y, double z, float pitch, float yaw) {
+		this.renderEntity(entity, x, y, z, pitch, yaw);
+	}
+
+	public void doRender(Entity entity, double x, double y, double z, float pitch, float yaw) {
+		this.renderEntity(entity, x, y, z, pitch, yaw);
+	}
+
+	@Override
+	protected ResourceLocation getEntityTexture(Entity e) {
+		if (e instanceof EntityEarthSage) return new ResourceLocation("simplemagic", "textures/mob/earthsage.png");
+		else if (e instanceof EntityLimboSage) return new ResourceLocation("simplemagic", "textures/mob/limbosage.png");
+		else if (e instanceof EntityFireSage) return new ResourceLocation("simplemagic", "textures/mob/firesage.png");
+	}
+}
